ae6f07ad12b9be85f7961284aff878852bfcb3c1
Ankit Mahajan <ankitm.block@gmail.com>
2019-06-25 22:01:46 +0530

    struct: Moved express code to separate lib folder

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,5 @@
  * Responds to a request with body content to demonstrate the app is running as expected.
  */
 module.exports = (req, res, next) => {
-  res.end(`Express Code Challenge Started`);
+  res.end(`Express Code Challenge Started`)
 }
